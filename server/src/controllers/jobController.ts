2574ba521f3796a8613b64c0261227856e793215
Rohan Sharma <rohansharma682002@gmail.com>
2026-09-21 21:56:38 +0530

    Added the get jobs api

--- server/src/controllers/jobController.ts
+++ server/src/controllers/jobController.ts
@@ -29,3 +29,15 @@ export const createJob = async (req: Request, res: Response) => {
     res.status(500).json({ error: error.message || "server error" });
   }
 };
+
+export const getJobs = async (req: Request, res: Response) => {
+  try {
+    const jobs = await Job.find();
+
+    res.status(200).json(jobs);
+  } catch (error: any) {
+    console.error(error);
+
+    res.status(500).json({ error: error.message || "server error" });
+  }
+};
--- server/src/routes/jobRoutes.ts
+++ server/src/routes/jobRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { createJob } from "../controllers/jobController.js";
+import { createJob, getJobs } from "../controllers/jobController.js";
 
 const router = express.Router();
 
 router.post("/", createJob);
+router.get("/", getJobs);
 
 export default router;
